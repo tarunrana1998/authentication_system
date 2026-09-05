31060616a1dfabf31c2bac7eb5bf87eafd98ed4e
Tarun Rana <tarun@91mobiles.com>
2026-09-05 08:38:38 +0530

    add cors middleware and configure allowed origins for frontend connectivity

--- src/app.js
+++ src/app.js
@@ -1,9 +1,20 @@
 import express from "express";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 import routes from "./routes/index.js";
 import { notFoundHandler, errorHandler } from "./middlewares/error.middleware.js";
 
 const app = express();
+
+// CORS configuration for frontend
+app.use(
+    cors({
+        origin: ["http://localhost:5173", "http://127.0.0.1:5173"],
+        credentials: true,
+        methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+        allowedHeaders: ["Content-Type", "Authorization", "x-access-token", "x-refresh-token", "token"],
+    })
+);
 
 // Core Middlewares
 app.use(express.json());
