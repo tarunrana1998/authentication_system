a9878eb713d3e5332f4547432ef36a58e55ef13b
Tarun Rana <tarun@91mobiles.com>
2026-09-05 11:01:51 +0530

    add production frontend URL to CORS allowlist

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ const app = express();
 // CORS configuration for frontend
 app.use(
     cors({
-        origin: ["http://localhost:5173", "http://127.0.0.1:5173"],
+        origin: ["http://localhost:5173", "http://127.0.0.1:5173","https://auth-red-seven.vercel.app/"],
         credentials: true,
         methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
         allowedHeaders: ["Content-Type", "Authorization", "x-access-token", "x-refresh-token", "token"],
